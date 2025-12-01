eb485075fcb4a2f81ddeafba9fe46e67c8f091ff
Egor <egor.pyuretskiy@gmail.com>
2025-12-01 10:07:09 +0300

    Implement initial dialogue with therapist.

--- src/drz.cpp
+++ src/drz.cpp
@@ -1,8 +1,27 @@
 #include "drz.h"
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 
 void launchGame()
 {
-	std::cout << "running the game..." << std::endl;
+	std::cout << std::setw(31) << "DR.Z\n";
+	std::cout << std::setw(38) << "CREATIVE COMPUTING\n";
+	std::cout << std::setw(40) << "MORRISTOWN, NEW JERSEY";
+	std::cout << std::string(4, '\n');
+
+	std::cout << "HELLO THERE, I'M YOUR COMPUTER THERAPIST.\n";
+	std::cout << "WHAT IS YOUR NAME? AND TELL ME SOMETHING ABOUT YOURSELF.\n";
+	std::cout << "HOWEVER, DON'T TYPE MORE THAN ONE LINE. I TIRE EASILY.\n";
+
+	std::string A, B;
+	std::cout << "? ", std::getline(std::cin, A);
+	std::cout << "WHAT DID YOU SAY YOUR NAME WAS AGAIN?\n";
+	std::cout << "? ", std::getline(std::cin, B);
+
+	std::cout << "HOW DO YOU FEEL TODAY?\n";
+
+	int C = 0, U = 0, V = 0;
+	std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 }
