c9d013f30c778ac1f3eb07eef7a7beed1d9727df
Egor <egor.pyuretskiy@gmail.com>
2025-12-08 23:20:26 +0300

    feat: add --random=N command line argument support.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include "drz.h"
 
-int main()
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main(int argc, char* argv[])
 {
-	launchGame();
-	return 0;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string_view arg(argv[i]);
+        if (arg.substr(0, 9) == "--random=")
+        {
+            size_t pos = arg.rfind('=');
+            if (pos != std::string_view::npos && pos + 1 < arg.length())
+            {
+                int setDigit = std::stoi(std::string(arg.substr(pos + 1)));
+                if (!(setDigit >= 1 && setDigit <= 10))
+                {
+                    std::cout << "Error...";
+                    return -1;
+                }
+                std::cout << setDigit << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    launchGame();
+    return 0;
 }
