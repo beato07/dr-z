b7b3c2c84b3deb2bcc19d5cf5b9caec9b961c6e2
Egor <egor.pyuretskiy@gmail.com>
2025-12-12 21:38:29 +0300

    chore: restructure DR.Z game loop logic.

--- src/drz.cpp
+++ src/drz.cpp
@@ -33,62 +33,51 @@ void launchGame()
 	std::cout << "HOW DO YOU FEEL TODAY?\n";
 
 	int C = 0, U = 0, V = 0;
-	std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
-
 	do
 	{
-		int Z = getRandomDigit();
+		std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 
-		if (U == Z) { Z = getRandomDigit(); }
-		if (V == Z) { Z = getRandomDigit(); }
+		int Z = 0;
+		do { Z = getRandomDigit(); } while (U == Z || V == Z);
 
+		U = Z;
 		switch (Z + 1)
 		{
 		case 1:
-			std::cout << "THAT'S VERY INTERESTING, TELL ME MORE.\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
+			std::cout << "THAT'S VERY INTERESTING, TELL ME MORE.\n\n";
 		case 2:
 			std::cout << "HAVE YOU FELT THIS WAY LONG?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 3:
 			std::cout << "DO YOU THINK THIS IS REASONABLE IN LIGHT OF YOUR INTERESTS?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 4:
 			std::cout << "DO YOUR FRIENDS FIND THIS ACCEPTABLE?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 5:
 			std::cout << "DO YOU FEEL COMFORTABLE WITH THIS FEELING?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 6:
 			std::cout << "DO YOU THINK THAT THIS IS A NORMAL FEELING?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 7:
 			std::cout << "WHY DO YOU THINK YOU FEEL THIS WAY?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 8:
 			std::cout << "HAVE YOU TALKED TO ANYONE ABOUT THIS?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 9:
 			std::cout << "WHY ARE YOU HERE?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		case 10:
 			std::cout << "ARE YOU SATISFIED WITH THE WAY YOUR IDEAS ARE DEVELOPING?\n\n";
-			std::cout << "? ", std::getline(std::cin, A), std::cout << "\n\n";
 			break;
 		}
 
 		U = Z;
 
-	} while (++C != 10);
+	} while (++C <= 10);
 
 	std::cout << "I THINK YOU ARE MAKING A GREAT ATTEMPT TO SOLVE YOUR\n";
 	std::cout << "DIFFICULTIES, AND I SEE NO NEED TO CONTINUE THIS\n";
