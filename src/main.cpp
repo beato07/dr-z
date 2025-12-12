a3f69edc9e29a7b54d36f2b2dcbf3e60b4371af7
Egor <egor.pyuretskiy@gmail.com>
2025-12-12 21:42:37 +0300

    simplify: remove argument parsing from main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
 #include "drz.h"
 
-#include <iostream>
-#include <string>
-#include <string_view>
-
-int main(int argc, char* argv[])
+int main()
 {
-    for (int i = 1; i < argc; ++i)
-    {
-        std::string_view arg(argv[i]);
-        if (arg.substr(0, 9) == "--random=")
-        {
-            size_t pos = arg.rfind('=');
-            if (pos != std::string_view::npos && pos + 1 < arg.length())
-            {
-                int setDigit = std::stoi(std::string(arg.substr(pos + 1)));
-                if (!(setDigit >= 1 && setDigit <= 10))
-                {
-                    std::cout << "Error...";
-                    return -1;
-                }
-                std::cout << setDigit << std::endl;
-                return 0;
-            }
-        }
-    }
-
     launchGame();
     return 0;
 }
